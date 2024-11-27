2df86579f38ec8ab5425b9d08ccf83091ea6df6d
Juan Gabriel Serrano Anaya <juangserrano2@gmail.com>
2024-11-26 20:18:05 -0500

    Subida de los comentarios y el inicio de la sección de consultas

--- spdn.py
+++ spdn.py
@@ -1,1 +1,51 @@
 # Archivo principal para el proyecto de Dinamisoft
+
+#Librerías para importar
+import os
+
+
+# Sección del menú Principal
+
+
+
+
+
+# 01. Sección de Creación de Pacientes
+
+
+
+
+# 02. Sección Ingresar datos de nivel de glucosa
+
+
+
+
+# 03. Sección Consultar registros de glucosa
+
+# Inicio del módulo de consultas
+print("*********************")
+print(" Módulo de Consultas")
+print("*********************")
+print("\nSeleccione la consulta que va a realizar")
+
+
+
+
+
+
+
+
+
+
+# 04 Sección Generar Reporte de niveles de glucosa por mes
+
+
+
+
+
+
+# 05. Sección Salir del Sistema
+
+
+
+
