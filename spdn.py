e2312dc0f86b4bb2f49675de430f80eb4abb48c9
Juan Gabriel Serrano Anaya <juangserrano2@gmail.com>
2024-11-28 20:06:23 -0500

    Se agrega if en la consulta de la cédula

--- spdn.py
+++ spdn.py
@@ -1,7 +1,13 @@
 # Archivo principal para el proyecto de Dinamisoft
 
 #Librerías para importar
+from ast import If
 import os
+
+
+# variables y listados para pruebas unitarias
+pacientes = [72293057,"Juan Gabriel","Serrano Anaya","Barranquilla"]
+
 
 
 # Sección del menú Principal
@@ -27,6 +33,11 @@ print("*********************")
 print(" Módulo de Consultas")
 print("*********************")
 print("\nSeleccione la consulta que va a realizar")
+consulta_cedula = int(input("Digite la cédula del paciente: "))
+If (consulta_cedula = pacientes):
+    print(pacientes)
+else:
+    print("No se encuentra información con la cédula ingresada.")
 
 
 
