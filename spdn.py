69dbcd4ff0f7450ca49b2badae4cfcd9c77e17c3
Samuel Puche <laurabornacelli02@gmail.com>
2024-11-30 16:12:45 -0500

    primero commit de los niveles de presion

--- spdn.py
+++ spdn.py
@@ -22,7 +22,48 @@ pacientes = [72293057,"Juan Gabriel","Serrano Anaya","Barranquilla"]
 
 
 # 02. Sección Ingresar datos de nivel de glucosa
+def clasificar_presion():
+  fecha = input("Ingrese la fecha (dd/mm/aaa): ")
+  hora = input("Ingrese la hora (hh:mm): ")
+  ayuna = input("¿Se encuentra en ayuna?: ")
+  #variables para pedir los niveles de presión
+  sistolica = float(input("Ingrese el nivel de presión sistólica: "))
+  diastolica = float(input("Ingrese el nivel de presión diastólica: "))
 
+  #Rangos de valores de presión para saber si es hipotensión
+  if sistolica < 91 and diastolica < 63:
+    print("Hipotensión\n¿Se programa la entrega? si")
+
+  #Rangos de valores de presión para saber si es ideal
+  elif 91 <= sistolica <= 134 and 63 <= diastolica <= 77:
+    print("Ideal\n¿Se programa la entrega? no")
+
+  #Rangos de valores de presión para saber si es normal
+  elif 134 <= sistolica <= 162 and 77 <= diastolica <= 105:
+    print("Normal\n¿Se programa la entrega? no")
+
+  #Rangos de valores de presión para saber si es normal-alta  
+  elif 162 <= sistolica <= 188 and 105 <= diastolica <= 119:
+    print("Normal-alta\n¿Se programa la entrega? si")
+
+  #Rangos de valores de presión para saber si es grado 1  
+  elif 188 <= sistolica <= 201 and 119 <= diastolica <= 126:
+    print("HTA Grado 1\n¿Se programa la entrega? si")
+
+  #Rangos de valores de presión para saber si es grado2
+  elif 201 <= sistolica <= 214 and 126 <= diastolica <= 146:
+    print("HTA Grado 2\n¿Se programa la entrega? si")
+
+  #Rangos de valores de presión para saber si es grado 3  
+  elif sistolica >= 214 and diastolica >= 146:  
+    print("HTA Grado 3\n¿Se programa la entrega? si")
+
+  #Rangos de valores de presión para saber si es hipertencio solo sitolica  
+  elif sistolica >= 152 and diastolica < 77:
+    print("Hipertensión Solo Sistólica\n¿Se programa la entrega? si")
+  else:
+    return "Valores fuera de rango"
+clasificar_presion()
 
 
 
