afde6d7be488637d740ed4eada2e7f1d0eaeb824
Santiago Ricaurte <Santiagoricaurte0215@gmail.com>
2024-12-02 14:42:32 -0500

    Agregue el menu de la aplicacion.

--- spdn.py
+++ spdn.py
@@ -12,8 +12,43 @@ pacientes = [72293057,"Juan Gabriel","Serrano Anaya","Barranquilla"]
 
 # Sección del menú Principal
 
+# Función para mostrar el menú principal con las opciones disponibles
+def menu():
+        print("\n******************** Bienvenido a S.P.D.N ********************")
+        print("\nA continuación, por favor elige una opción: ")
 
+        print("1). Registrar paciente.")  
+        print("2). Ingresar datos de nivel de Presion: ")  
+        print("3). Consultar mis registros de Presion: ")  
+        print("4). Generar un reporte de niveles de Presion por mes")  
+        print("5). Salir del sistema.")  
 
+# Función principal para manejar el menú
+def menu_principal():
+
+        while True:  # Bucle  para mostrar el menú hasta que el usuario decida salir
+                menu()  # Llama a la función menu para mostrar las opciones
+                try:
+                        opcion = int(input("Elige una opción del 1 al 5: "))  
+                except ValueError:  # Maneja el error si el usuario no ingresa un número válido
+                        print ("Por favor ingresa un número válido")
+                        continue  # Regresa al inicio del bucle para mostrar el menú nuevamente
+                if opcion == 1:
+                                registropacientes()  # Llama a la función para registrar pacientes
+                elif (opcion == 2):
+                                clasificar_presion()  # Llama a la función para ingresar datos de presión
+                elif (opcion == 3):
+                                Registros_presion()  # Llama a la función para consultar registros de presión
+                elif (opcion == 4):
+                                Reporte()  # Llama a la función para generar reportes
+                elif (opcion == 5):
+                                print ("Saliendo del sistema!")  # Mensaje de salida
+                                break  # Rompe el bucle para terminar el programa
+                else:
+                                print ("Solo hay opciones del 1 al 5")  # Mensaje de error para opciones fuera de rango
+                
+# Llama a la función principal para iniciar el programa
+menu_principal()
 
 
 # 01. Sección de Creación de Pacientes
